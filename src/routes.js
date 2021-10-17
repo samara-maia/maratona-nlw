ba312bf4a55090cd96e3a0ad5ca536724e6c64f4
Samara-rbd <maiareismaia@gmail.com>
2021-10-16 21:50:52 -0300

    pequenas alterações

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const routes = express.Router();
 
-const views = __dirname + "/views/";
+const views = __dirname + "/views/"
 
 //caminho baviews 
 const profile = {
-    name: "samara",
+    name: "Samara",
     avatar: "https://github.com/Samara-rbd.png",
     "monthly-budget": 3000,
     "days-per-week": 5,
@@ -20,14 +20,14 @@ const profile = {
       name: "Pizzaria Guloso",
       "daily-hours" : 2,
       "total-hours" : 60,
-      created_at: Date.now()
+      created_at: Date.now(),
     },
     {
     id: 2,
       name: "OneTwo Project",
       "daily-hours" : 3,
       "total-hours" : 47,
-      created_at: Date.now()
+      created_at: Date.now(),
     }
   ];
   
@@ -63,27 +63,27 @@ routes.get('/', (req, res) => {
     }
   }) 
   
-
   return res.render(views + "index", {jobs: updatedJobs})
+  
 })
 
 routes.get('/job', (req, res) => res.render(views +  "job"))
 
 routes.post('/job', (req, res) => {
-    const lastId = jobs[jobs.length -1]?.id || 1;
+    const lastId = jobs[jobs.length - 1]?.id || 1;
 
     jobs.push({
         id: lastId + 1,
         name: req.body.name,
         "daily-hours" : req.body["daily-hours"],
         "total-hours" : req.body["total-hours"],
-          created_at: Date.now()//atribuindo data de hoje
+         created_at: Date.now()//atribuindo data de hoje
     })
     return res.redirect('/')
 })   
 
-routes.get('/job/edit', (req, res) => res.render(views +"job-edit"))
+routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 
 routes.get('/profile', (req, res) => res.render(views + "profile" , { profile }))
 
-module.export = routes;
+module.exports = routes;
